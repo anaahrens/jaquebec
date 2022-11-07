a273721edc59e97b55c8c36bcff065d27b20133c
Ana Paula Ahrens <105805241+anaahrens@users.noreply.github.com>
2022-11-07 11:44:56 +0100

    main Master

---
+++ ExemploForArrays.java
@@ -0,0 +1,10 @@
+public class ExemploForArrays {
+    
+    public static void main(String[] args) {
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O alno indice x= " + x + " é " + alunos[x]);
+        }
+    }
+}
